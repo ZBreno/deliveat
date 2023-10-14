f5f8f525afac3494e8ef8cdd23869163c33d9138
Zbreno <brenosoares9533@gmail.com>
2023-10-14 17:35:31 -0300

    add: ui kitten

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import Welcome from "./src/screens/WelcomeScreen";
+import * as eva from "@eva-design/eva";
+import {
+  ApplicationProvider,
+  Button,
+  Layout,
+  Text,
+} from "@ui-kitten/components";
+import React, { useState } from "react";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Layout style={styles.container}>
+        <StatusBar style="auto" />
+        <Welcome />
+        <Button onPress={() => alert("mindé papai")}>BUTAO</Button>
+      </Layout>
+    </ApplicationProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/screens/WelcomeScreen.tsx
@@ -0,0 +1,18 @@
+import { View } from "react-native";
+import React from "react";
+import { Text } from "@ui-kitten/components";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+
+
+export default function Welcome() {
+  
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  );
+}
